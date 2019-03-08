092a65e1513d38850f57f21a9adf1f13d3a1c6b9
Scott Lu <Scott.Lu@ff.com>
2019-03-08 09:24:04 -0800

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # spring-cloud-demo
+# Folder Structure
+## ./config 
+config file
+## ./config-service
+provide central config for other modules
+## ./eureka-service 
+provide eureka service registration & discovery
+## ./reservation-service
+provide restful api service, when it start it will register it's self to eureka
+## ./reservation-client
+will talk to eureka, to find the reservation service .
+
+# SETUP
+## 1. git config repo
+goto folder "config", run command:
+```bash
+git init
+git add .
+git commit -m "initial commit"
+```
+this will create local git repo for config file
+## 2. run config service
+in folder config-service
+```bash
+#cd config-service
+mvn spring-boot:run
+```
+you can check applications properties by below links:
+```bash
+http://localhost:8888/reservation-service/master
+http://localhost:8888/reservation-client/master
+http://localhost:8888/eureka-service/master
+```
+## 3. start eureka
+
+```bash
+#cd eureka-service
+
+mvn spring-boot:run
+```
+you can check goto, below address to see the registered services:
+```
+http://localhost:8761
+```
+## 4. start reservation service:
+```bash
+#cd reservation-service
+mvn spring-boot:run
+```
+this service provide api via "spring rest reposity", sample links:
+```bash
+http://localhost:8000/reservations  #all reservations
+http://localhost:8000/reservations/1 #single reservation
+http://192.168.103.117:8000/reservations/search/by-name?rn=Scott #search by name
+```
+
+## 5. start reservation client:
+```bash
+#cd reservation-client
+mvn spring-boot:run
+```
+check result by :
+```bash
+http://localhost:9999/reservations/names
+```
+
--- reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -1,52 +1,43 @@
 package com.example.reservationclient;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@EnableZuulProxy
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class ReservationClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReservationClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
 
@@ -55,37 +46,36 @@ public class ReservationClientApplication {
 @RequestMapping("/reservations")
 class ReservationApiGetwayController {
 
+	@Autowired
+	EmployeeProxy proxy;
 
-    @Autowired
-    EmployeeProxy proxy;
+	@GetMapping("/names")
+	public List<String> getReservationNames() throws IOException {
+		String str = proxy.findAll();
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode rootNode = objectMapper.readTree(str);
+		JsonNode rs = rootNode.path("_embedded").path("reservations");
 
-    @GetMapping("/names")
-    public List<String> getReservationNames() throws IOException {
-        String str = proxy.findAll();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(str);
-        JsonNode rs = rootNode.path("_embedded").path("reservations");
+		return Arrays.asList(objectMapper.treeToValue(rs, Reservation[].class)).stream()
+				.map(Reservation::getReservationName).collect(Collectors.toList());
+	}
 
-        return Arrays.asList(objectMapper.treeToValue(rs, Reservation[].class)).stream()
-                .map( Reservation::getReservationName).collect(Collectors.toList());
-    }
-    @GetMapping(path = {"/",""})
-    public List<Reservation> getReservations() throws IOException {
-        String str = proxy.findAll();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(str);
-        JsonNode rs = rootNode.path("_embedded").path("reservations");
+	@GetMapping(path = { "/", "" })
+	public List<Reservation> getReservations() throws IOException {
+		String str = proxy.findAll();
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode rootNode = objectMapper.readTree(str);
+		JsonNode rs = rootNode.path("_embedded").path("reservations");
 
-        return Arrays.asList(objectMapper.treeToValue(rs, Reservation[].class));
-    }
+		return Arrays.asList(objectMapper.treeToValue(rs, Reservation[].class));
+	}
 
 }
 
-
 @FeignClient(name = "reservation-service")
 interface EmployeeProxy {
-    @RequestMapping("/reservations")
-    public String findAll();
+	@RequestMapping("/reservations")
+	public String findAll();
 
 }
 
@@ -93,7 +83,5 @@ interface EmployeeProxy {
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Reservation {
-    String reservationName;
+	String reservationName;
 }
-
-
